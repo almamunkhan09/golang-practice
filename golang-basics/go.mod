238b5965d550d3fe76fb4187f304583d6dcac372
Al Mamun Khan <almamunkhan09@gmail.com>
2023-05-13 18:16:32 +0200

    Start learning go from Nana

---
+++ golang-basics/go.mod
@@ -0,0 +1,3 @@
+module golang-bascis
+
+go 1.20
---
+++ golang-basics/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+
+func main ()  {
+
+	firstName := "Al Mamun"
+	lastName := "Khan"
+	const language = "Golang"
+	fmt.Printf("Hello! Good Afternoon %s %s\n", firstName,lastName)
+	fmt.Printf("Welcome to %s learning\n ", language)
+
+}
